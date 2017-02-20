957b9c0381976d1a7d7a4b874752e193cfa3cb89
Xqf5683 <734086455@qq.com>
2017-02-21 00:29:51 +0800

    add a pages about duty

---
+++ application/views/duty/footer.php
@@ -0,0 +1,7 @@
+	
+
+	<script src="<?php echo base_url(); ?>js/duty/jquery.min.js"></script>
+	<script src="<?php echo base_url(); ?>js/duty/btn.js"></script>
+
+</body>
+</html>
---
+++ application/views/duty/index.php
@@ -0,0 +1,115 @@
+	<div class="header">
+		<img id="image-hh"  src="<?php echo base_url(); ?>images/duty/hh.png"/>		
+	</div>
+	<div class="content">
+		<div class="content-left">
+			<div class="content-left-top">
+				<a class="show-layer" onclick="False()" data-show-layer="hw-layer" role="button">
+					<img id="image-tuan"  src="<?php echo base_url(); ?>images/duty/tuan.png" onclick="False()" />
+				</a>		
+			</div>
+			<div class="hw-overlay" id="hw-layer">
+				<div class="hw-layer-wrap">
+					<button type="button" class="close hwLayer-close" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+					<div class="row">
+						<div class="images-icon"></div>
+						<div class="content-left-foot">
+							<ul>
+								<li><a href="#"><div class="mulu">抢班</div></a></li>
+								<li><a href="#"><div class="mulu">签到</div></a></li>
+								<li><a href="#"><div class="mulu">通讯录</div></a></li>
+								<li><a href="#"><div class="mulu">个人信息</div></a></li>
+								<li><a href="#"><div class="mulu">值班统计</div></a></li>
+								<li><a href="#"><div class="mulu">退班申请</div></a></li>
+								<li><a href="#"><div class="mulu">交流论坛</div></a></li>
+								<li><a href="#"><div class="mulu">行政排班</div></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="content-left-time">
+				<dl>
+					<dd>&nbsp;8:00 10:00</dd>
+					<dd>10:00 12:00</dd>
+					<dd>12:00 14:00</dd>
+					<dd>14:00 16:00</dd>
+					<dd>16:00 18:00</dd>
+					<dd>18:00 21:00</dd>
+				</dl>
+			</div>
+		</div>
+		<div class="content-right">
+			<div class="content-right-top">
+				<img id="image-week"  src="<?php echo base_url(); ?>images/duty/week.png" />		
+			</div>
+			<div class="content-right-foot">
+				<div class="baoban">
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+				</div>
+				<div class="baoban">
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+				</div>	 
+				<div class="baoban">
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+				</div>
+				<div class="baoban">
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+				</div>
+				<div class="baoban">
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+				</div>
+				<div class="baoban">
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+					<p class="choice"><span>1</span></p>
+				</div>				
+			</div>
+			
+		</div>	
+	</div>
+	<div class="foot">
+		<div class="foot-left">
+			<p id="tongzhi">注意通知：</p>
+		</div>
+		<div class="foot-right">
+			<p id="zhuyi">每天分为六个班次，每次值班时间为2~3小时,每次报班人数最多三人。寒暑假及特殊周期值班时按学校易班领导小组统一决定。</p>	
+		</div>
+	</div>	
